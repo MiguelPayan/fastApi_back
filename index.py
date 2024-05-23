6ef52940096296a2362a06de849b1ae2e2d0fd82
CarlosGuardado <L20550382@chihuahua2.tecnm.mx>
2024-05-23 02:17:43 -0600

    a

--- index.py
+++ index.py
@@ -1,3 +1,45 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pandas as pd
+
+# Modelo Pydantic para validar el nuevo registro
+class NewRecord(BaseModel):
+    Nombre: str
+    Edad: int
+    Equipo: str
+    Rendimiento: int
+    Potencial: int
+    valor_mercado: float
+
+# Inicializar la aplicación FastAPI
+app = FastAPI()
+
+
+app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes, puedes restringirlo a dominios específicos
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+# Endpoint para obtener todos los jugadores
+@app.get("/jugadores")
+def read_data():
+    try:
+        # Leer el archivo CSV
+        data = pd.read_csv('JugadoresMayorMenos.csv')
+        
+        # Convertir el DataFrame a una lista de diccionarios
+        data = data.to_dict(orient="records")
+        
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 # Endpoint para agregar un nuevo registro al CSV
 @app.post("/jugadores/")
 def add_record(record: NewRecord):
@@ -56,3 +98,20 @@ async def eliminar_jugador(nombre: str):
         return {"mensaje": "Jugador eliminado correctamente"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+# Endpoint para obtener jugadores por equipo
+@app.get("/equipos/{equipo}", tags=["Equipos"])
+async def get_jugadores_por_equipo(equipo: str):
+    jugadores = data[data['Equipo'] == equipo]
+    if jugadores.empty:
+        raise HTTPException(status_code=404, detail="Equipo no encontrado")
+    return jugadores.to_dict(orient="records")
+
+# Endpoint para obtener jugadores por rango de edad
+@app.get("/jugadores/edad/", tags=["Jugadores"])
+async def get_jugadores_por_edad(edad_min: int, edad_max: int):
+    jugadores = data[(data['Edad'] >= edad_min) & (data['Edad'] <= edad_max)]
+    if jugadores.empty:
+        raise HTTPException(status_code=404, detail="No se encontraron jugadores en el rango de edad especificado")
+    return jugadores.to_dict(orient="records")
